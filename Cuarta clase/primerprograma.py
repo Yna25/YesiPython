eb80ae004236f3639e5f076c80328a7840f4b41d
Yesica Peña Guerrero <yuye3111@gmail.com>
2025-12-27 22:03:50 -0500

    Cuarta clase

---
+++ Cuarta clase/primerprograma.py
@@ -0,0 +1,9 @@
+#Calculadora que reciba numeros pero que no reciba lo que sea.
+#Tenga todos los tipos de errores
+#Tenga suma, resta, multiplicación, división, exponenciación
+#Operacion que rciba 2 numeros y la operacion que va a realizar
+
+x= input ("Ingresa el primer numero")
+y= input ("Ingresa el segundo numero")
+op= ("Ingresa la operación a realizar (suma (+), resta (-), multiplicación (*), división (/), exponenciación (**))")
+
